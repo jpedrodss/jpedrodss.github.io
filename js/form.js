5ef134a5937ce4eb07c6f38f0e0280d25d137c95
João Pedro <61585286+jpedrodss@users.noreply.github.com>
2022-09-04 21:27:08 -0300

    added js

---
+++ js/form.js
@@ -0,0 +1,10 @@
+// Função para envio de e-mail
+function submitEmail() {
+    alert("Seu e-mail foi enviado com sucesso! Aguarde nosso contato.");
+}
+
+// Função para limpar os campos do formulário
+function clear() {
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+}
